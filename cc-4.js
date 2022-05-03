7b471851a01a24e6a45548f373e06532ef4f4d24
Reginald Devarel <reginalddevarel@MacBook-Pro.local>
2022-05-03 11:58:44 -0400

    I tried

--- cc-4.js
+++ cc-4.js
@@ -16,12 +16,6 @@ console.log(swim());
 
 //MEDIUM: Write a function that would allow you to do this:
 
-// var sharePizza = function cutPizzaSlices(num){
-//  var num = 8;
-//  return num / 2;
-
-// }
-
 var sharePizza = function cutPizzaSlices(slice, num) {
     var num = 8;
     var result = num / slice;
@@ -31,3 +25,33 @@ var sharePizza = function cutPizzaSlices(slice, num) {
 
 
 console.log(sharePizza(2));
+
+
+//Hard: 
+
+var Person = (function() {
+    var private = new WeakMap();
+
+    function Person(ssn, name) {
+     this.name = name;
+      Person.prototype.getName = function() {
+      return `${this.name}`;
+     }
+        
+        var privateProperties = {
+            ssn: ssn
+        };
+        private.set(this, privateProperties);
+    }
+
+    Person.prototype.getSSN = function() {
+        return private.get(this).ssn;
+    };
+
+
+    return Person;
+}());
+
+var reggie = new Person(485297730, "Reginald");
+console.log('Person Name: ' + reggie.getName() + ' Person SSN: ' + Object.getOwnPropertyNames(reggie) );
+//Couldn't get the SSN to stop displaying name, I know it had something to with the Object.getOwnPropertyNames
