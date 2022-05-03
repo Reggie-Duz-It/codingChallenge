3e9483d8bde6a30380d124111e8466842c43a4a3
Reginald Devarel <reginalddevarel@MacBook-Pro.local>
2022-05-03 01:01:36 -0400

    Medium

--- cc-4.js
+++ cc-4.js
@@ -15,6 +15,19 @@ console.log(swim());
 
 
 //MEDIUM: Write a function that would allow you to do this:
-var sharePizza = function cutPizzaSlices(){
 
+// var sharePizza = function cutPizzaSlices(num){
+//  var num = 8;
+//  return num / 2;
+
+// }
+
+var sharePizza = function cutPizzaSlices(slice, num) {
+    var num = 8;
+    var result = num / slice;
+    return `Each person gets ${result.toFixed(2)} slices of Pizza`
 }
+
+
+
+console.log(sharePizza(2));
