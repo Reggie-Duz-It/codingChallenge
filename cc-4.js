8feb6c0ef15bdcd4c0838ab78feba51a1218e40e
Reginald Devarel <reginalddevarel@MacBook-Pro.local>
2022-05-02 22:28:01 -0400

    cc-4

---
+++ cc-4.js
@@ -0,0 +1,20 @@
+//EASY: Write a function that would allow you to do this:
+
+var run = function (running) {
+    var running = "running";
+    return `Today's excercise: ${running}`
+}
+
+var swim = function (swimming) {
+    var swimming = "swimming";
+    return `Today's excercise: ${swimming}`
+}
+
+console.log(run());
+console.log(swim());
+
+
+//MEDIUM: Write a function that would allow you to do this:
+var sharePizza = function cutPizzaSlices(){
+
+}
